0a075f4102e66dc7d4ea5b290213922e10099407
Julian <julian.huber2@t-online.de>
2023-11-01 20:33:48 +0100

    Made things work

--- graph_generation/graph_generation.cpp
+++ graph_generation/graph_generation.cpp
@@ -1,13 +1,17 @@
 #include <ogdf/fileformats/GraphIO.h>
 #include <ogdf/basic/graph_generators.h>
+#include <ogdf/layered/SugiyamaLayout.h>
+#include <ogdf/layered/OptimalRanking.h>
+#include <ogdf/layered/MedianHeuristic.h>
+#include <ogdf/layered/OptimalHierarchyLayout.h>
 
 using namespace ogdf;
 
 int randomGraphs() {
-    for (int i = 1; i <= 20; i++)
+    for (int i = 1; i <= 20; ++i)
     {
         Graph G;
-        randomGraph(G, 3 * i, (i+1)^2);
+        randomSimpleConnectedGraph(G, 2000 * i, 2001 * i);
         GraphIO::write(G, "random" + std::to_string(i) + ".gml", GraphIO::writeGML);
     }
 
--- main.cpp
+++ main.cpp
@@ -11,46 +11,22 @@ using namespace ogdf;
 
 int main()
 {
-    Graph G;
-    GraphAttributes GA(G,
-                       GraphAttributes::nodeGraphics | GraphAttributes::edgeGraphics);
-
-    const int LEN = 5;
-    node nodes[5];
-    for (int i = 0; i < LEN; ++i)
-    {
-        nodes[i] = G.newNode();
-    }
-    for (int i = 0; i < LEN; ++i)
-    {
-        for (int j = 0; j < LEN; ++j)
-        {
-            if (j != i)
-            {
-                G.newEdge(nodes[i], nodes[j]);
-            }
-        }
+    for (int i = 1; i < 21; ++i) {
+        Graph G;
+        GraphAttributes GA(G,
+                           GraphAttributes::nodeGraphics |
+                           GraphAttributes::edgeGraphics |
+                           GraphAttributes::nodeLabel |
+                           GraphAttributes::edgeStyle |
+                           GraphAttributes::nodeStyle |
+                           GraphAttributes::nodeTemplate);
+        GraphIO::read(GA,G, "graph_generation/random" + std::to_string(i) + ".gml", GraphIO::readGML);
+        BoyerMyrvold b;
+        bool planar = b.isPlanar(G);
+        printf("Graph %d: %d\n", i, planar);
     }
 
-    SugiyamaLayout SL;
-    SL.setRanking(new OptimalRanking);
-    SL.setCrossMin(new MedianHeuristic);
- 
-    OptimalHierarchyLayout *ohl = new OptimalHierarchyLayout;
-    ohl->layerDistance(30.0);
-    ohl->nodeDistance(25.0);
-    ohl->weightBalancing(0.8);
-    SL.setLayout(ohl);
- 
-    SL.call(GA);
-    GraphIO::write(GA, "output-unix-history-hierarchical.gml", GraphIO::writeGML);
-    GraphIO::write(GA, "output-unix-history-hierarchical.svg", GraphIO::drawSVG);
 
-    BoyerMyrvold bm;
-
-    bool isPlanar = bm.isPlanar(G);
-
-    printf("Planar: %d\n", isPlanar);
 
     return 0;
 }
